b98f3761333b70faf102a90d8c57b9b675c9c497
xYamii <32822766+xYamii@users.noreply.github.com>
2020-03-03 21:45:25 +0100

    loadfile instead of loadurl

--- index.js
+++ index.js
@@ -1,25 +1,22 @@
 const electron = require("electron");
 const { app, BrowserWindow } = electron;
 
+let win = null;
+
 app.on("ready", () => {
-  let win = new BrowserWindow({
+  win = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
       nodeIntegration: true
     }
   });
-  win.loadURL(`file://${__dirname}/index.html`);
+  win.loadFile("index.html");
 });
+
 app.on("window-all-closed", app.quit);
+
 app.on("before-quit", () => {
   mainWindow.removeAllListeners("close");
   mainWindow.close();
 });
-exports.openWindow = filename => {
-  let win = new BrowserWindow({
-    width: 800,
-    height: 600
-  });
-  win.loadURL(`file://${__dirname}/${filename}.html`);
-};
